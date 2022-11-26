75f9c5dc651db2225790caf64dc6c2b66aaaa870
Levent Sencer Şahin <l.sencer.sahin@gmail.com>
2022-11-27 02:55:19 +0300

    ReactNative ile start verildi.

---
+++ storage/db.js
@@ -0,0 +1,72 @@
+export default [
+    {
+        "id": 1,
+        "name": "Kurban Bayramı",
+        "count": "88K",
+        "date": "21.08.2018",
+        "photo": require("../assets/kurban_bayrami.png")
+    },
+    {
+        "id": 2,
+        "name": "Doğum Günü",
+        "count": "1K",
+        "date": "14.11.2018",
+        "photo": require("../assets/dogum_gunu.png")
+    },
+    {
+        "id": 3,
+        "name": "Mevlid Kandili",
+        "count": "68K",
+        "date": "19.11.2018",
+        "photo": require("../assets/kandil.png")
+    },
+    {
+        "id": 4,
+        "name": "Öğretmenler Günü",
+        "count": "8K",
+        "date": "24.11.2018",
+        "photo": require("../assets/ogretmenler_gunu.png")
+    },
+    {
+        "id": 5,
+        "name": "Yılbaşı",
+        "count": "73K",
+        "date": "01.01.2019",
+        "photo": require("../assets/yilbasi.png")
+    },
+    {
+        "id": 6,
+        "name": "Sevgililer Günü",
+        "count": "1K",
+        "date": "14.02.2019",
+        "photo": require("../assets/sevgililer_gunu.png")
+    },
+    {
+        "id": 7,
+        "name": "Anneler Günü",
+        "count": "1K",
+        "date": "12.05.2019",
+        "photo": require("../assets/anneler_gunu.png")
+    },
+    {
+        "id": 8,
+        "name": "Doğum Günü",
+        "count": "1K",
+        "date": "31.05.2018",
+        "photo": require("../assets/dogum_gunu.png")
+    },
+    {
+        "id": 9,
+        "name": "Ramazan Bayramı",
+        "count": "88K",
+        "date": "05.06.2019",
+        "photo": require("../assets/ramazan_bayrami.png")
+    },
+    {
+        "id": 10,
+        "name": "Babalar Günü",
+        "count": "1K",
+        "date": "16.06.2019",
+        "photo": require("../assets/babalar_gunu.png")
+    }
+]
